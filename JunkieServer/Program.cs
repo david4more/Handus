4c27e1ceed8d22ac82aac0fadfe42eb33bec3a4c
OdariukSofiia <sonya.odaruk@gmail.com>
2026-05-08 15:01:27 +0300

    Expand stored user data

--- JunkieServer/Program.cs
+++ JunkieServer/Program.cs
@@ -243,6 +243,8 @@ public class User
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Email { get; set; } = null!;
     public float PositionX { get; set; }
     public float PositionY { get; set; }
 }
